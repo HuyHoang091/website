b0e84e74b507203e02bdf7348ff5dac57cb67785
Clechannd <brlt091@gmail.com>
2025-10-06 21:57:55 +0700

    update

--- llm.bat
+++ llm.bat
@@ -6,3 +6,5 @@ start cmd /k "rag-env\Scripts\activate && python app.py"
 cd /d C:\xampp\htdocs\hoanghuy\project\viton
 
 start cmd /k "viton-env\Scripts\activate && cd /d C:\xampp\htdocs\hoanghuy\project\llm-rag\langchain-service && python main.py"
+
+start cmd /k "viton-env\Scripts\activate && cd /d C:\xampp\htdocs\hoanghuy\project\nlm-search && uvicorn main:app --host 0.0.0.0 --port 8000"
---
+++ ngrok/ngrok.bat
@@ -0,0 +1,5 @@
+cd /d C:\xampp\htdocs\hoanghuy\project\ngrok
+
+start cmd /k "ngrok start web --config C:\xampp\htdocs\hoanghuy\project\ngrok\ngrok1.yml"
+
+start cmd /k "ngrok start web --config C:\xampp\htdocs\hoanghuy\project\ngrok\ngrok2.yml"
